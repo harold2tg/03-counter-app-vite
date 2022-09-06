369adb086890164eccda8128c6c687b89dbc5686
Harold Torres Gallo <harold2tg@gmail.com>
2022-09-06 15:31:49 -0500

    hook clase 1

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,16 +1,19 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types'
 
 export const CounterApp = ({value}) =>{
     
-    function handleAdd(event,value){
-        console.log(value)
+    const [ counter, setCounter ] = useState(0)
+
+    function handleAdd(){
+        setCounter(counter + 1)
     }
 
     return(
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
-            <button onClick={ (event)=> handleAdd(event,value)}>
+            <h2> { counter } </h2>
+            <button onClick={ handleAdd }>
             +1
             </button>
         </>
