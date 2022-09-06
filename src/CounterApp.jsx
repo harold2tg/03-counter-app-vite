6729baa9c5955e8c1fe194988accc0026417c7e2
Harold Torres Gallo <harold2tg@gmail.com>
2022-09-06 15:49:17 -0500

    Agregando las funciones handleSubstract y handleReset con hook

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -9,13 +9,22 @@ export const CounterApp = ({value}) =>{
         setCounter( (counter)=> counter + 1)
     }
 
+    function handleSubstract(){
+        setCounter( (counter)=> counter - 1)
+    }
+
+    function handleReset(){
+    setCounter( (counter)=> counter=0)
+    }
+
     return(
         <>
             <h1>CounterApp</h1>
             <h2> { counter } </h2>
-            <button onClick={ handleAdd }>
-            +1
-            </button>
+            <button onClick={ handleAdd }>+1</button>
+            <button onClick={ handleSubstract }>-1</button>
+            <button onClick={ handleReset }>Reset</button>
+            
         </>
     );
 
