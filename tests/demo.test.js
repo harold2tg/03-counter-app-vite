4c504e7f3343947bba5b644e26b403cd49b593f8
Harold Torres Gallo <harold2tg@gmail.com>
2022-09-06 22:05:53 -0500

    Descripcion de la prueba

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,5 +1,5 @@
 
-
+describe('Pruebas en ,<demoComponent />', ()=>{
 test('esta prueba no puede fallar', () => { 
     // 1.  Iniciacion 
     const mensaje1 = 'Hola mundo';
@@ -9,4 +9,6 @@ test('esta prueba no puede fallar', () => {
 
     // 3. Observar el comportamiento ... esperado
     expect(mensaje1).toBe (mensaje2);
- })
+ });
+
+});
