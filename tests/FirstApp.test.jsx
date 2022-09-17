f68fc2d48694bc07675089b3703060304ac4661c
Harold Torres Gallo <harold2tg@gmail.com>
2022-09-17 18:07:47 -0500

    Probando la existencia de h1

--- tests/FirstApp.test.jsx
+++ tests/FirstApp.test.jsx
@@ -8,6 +8,17 @@ describe('Probando componente FirstApp', () => {
         const subtitulo = 'Soy ingeniero de sistemas'
         const nombre = 'Hola soy Hacreado por isaias'
 
-        render(<FirstApp title={titulo}/>)
+        const {container } = render(<FirstApp title={titulo}/>)
+        expect (container).toMatchSnapshot();
+
      })
+     test('debe mostrar el titulo en h1', () => { 
+        const titulo = 'Hola soy Harold'
+        const {container, getByText } = render(<FirstApp title={titulo}/>)
+        expect (getByText(titulo)).toBeTruthy();
+        const h1 = container.querySelector('h1')
+        console.log(h1.innerHTML)
+        expect(h1.innerHTML).toContain(titulo)
+
+      })
  })
